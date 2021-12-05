140aafecfdb2279093b0868290c53ee1b910fc27
Claudia Hong <62270305+claudiahaa@users.noreply.github.com>
2021-12-05 22:51:46 +0900

    Add files via upload
    Conding Practice Java_5  특정문자 뒤집기

---
+++ Practice5.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Practice5 {
+	
+	public String solution(String str) {
+		
+		String answer;
+		
+		char[] s = str.toCharArray();
+		
+		int lt=0;
+		int rt=str.length()-1;
+		
+		while(lt<rt) {
+			
+			if (!Character.isAlphabetic(s[lt])) lt++;
+			else if (!Character.isAlphabetic(s[rt])) rt--;
+			else {
+				char tmp = s[lt];
+				s[lt] = s[rt];
+				s[rt] = tmp;
+				lt++;
+				rt--;
+			}
+		}
+		
+		answer = String.valueOf(s);
+		return answer;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Practice5 T = new Practice5();
+		Scanner kb = new Scanner(System.in);
+		
+		String str = kb.next();
+		System.out.print(T.solution(str));
+		
+	}
+
+}
